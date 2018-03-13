5d2af385a06d86a12305b0f95321bf21647fcaeb
rstar2 <rstar2@abv.bg>
2018-03-13 17:44:21 +0200

    Add server code

---
+++ index.js
@@ -0,0 +1,58 @@
+const process = require('process');
+const path = require('path');
+const fs = require('fs');
+
+const _ = require('lodash');
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const subscriptionsFile = path.join(__dirname, 'subscriptions.txt');
+
+// this will create (if not existing) and open the file for appending
+const subscriptionsStream = fs.createWriteStream(subscriptionsFile, { flags: 'a' });
+
+const app = express();
+
+// By default express-static will sends the specified directory index file (defaults to 'index.html')
+// so for "/" - "/index.html" will be served
+app.use(express.static('public'));
+
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
+
+app.post('/subscribe', (req, res) => {
+    let email = req.body.email;
+    email = validateEmail(email);
+    if (email) {
+        subscriptionsStream.write(new Date().toLocaleString() + '\t-\t');
+        subscriptionsStream.write(email + '\n');
+    }
+
+    res.json({ success: true });
+});
+
+app.listen(process.env.PORT || 3000, () => console.log('Listening on port 3000!'));
+app.on('error', () => {
+    subscriptionsStream.close();
+    process.exit(1);
+})
+
+
+const validateEmail = (email) => {
+    let validEmail = false;
+
+    if (_.isString(email)) {
+        email = email.trim();
+
+        // TODO: add email validation lib
+        if (email) {
+            validEmail = email;
+        }
+    }
+
+    return validEmail;
+}
